139cd6d27c6b07fd2986a110934383064f310c59
Claire Deneau <claire.deneau@etu.univ-orleans.fr>
2024-02-15 08:39:42 +0100

    Constructeur journee

--- tp2/Journee.java
+++ tp2/Journee.java
@@ -1,3 +1,23 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Journee{
-    private 
+    private Map<Style, Integer> matin;
+    private Map<Style, Integer> aprem;
+    private Map<Style, Integer> soiree;
+    
+    
+    public Journee(Style matin, int mat, Style aprem, int ap, Style soiree, int soir) {
+        this.matin = new HashMap<>();
+        this.matin.put(matin, mat);
+        this.aprem = new HashMap<>();
+        this.aprem.put(aprem, ap);
+        this.soiree = new HashMap<>();
+        this.soiree.put(soiree, soir);
+    }
+
+    public int score_moment(){
+        
+    }
+    
 }
